2e82f153ffe114419cd4e11497ad0dfef80408b9
DarkenedFate <darkenedfate92@gmail.com>
2016-06-08 00:32:39 -0400

    added the ability to save servers for easy connecting

---
+++ Shelltest1/AddServerWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Shelltest1
+{
+    /// <summary>
+    /// Interaction logic for AddServerWindow.xaml
+    /// </summary>
+    public partial class AddServerWindow : Window
+    {
+        public AddServerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var file = new FileInfo("settings.bbq");
+            if (!file.Exists) file.Create().Close();
+
+            var settingString = $"{tbHostname.Text}:{tbUsername.Text}:{pbPassword.Password}\n";
+
+            File.AppendAllText(file.FullName, settingString);
+        }
+    }
+}
--- Shelltest1/MainWindow.xaml.cs
+++ Shelltest1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,14 @@ namespace Shelltest1
         public MainWindow()
         {
             InitializeComponent();
+            LoadServers();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            using (SshClient linux = new SshClient("72.185.188.0", "mdeering", "Lolpops123"))
+            var currentServer = ServerSettingses.FirstOrDefault(x => x.Hostname == cmbServers.Text);
+
+            using (SshClient linux = new SshClient(currentServer?.Hostname, currentServer?.Username, currentServer?.Password))
             {
                 linux.Connect();
                 if (!linux.IsConnected)
@@ -42,15 +46,51 @@ namespace Shelltest1
                     linux.RunCommand("cd /home/mdeering");
                     linux.RunCommand("./update.sh");
                 }
+                linux.Disconnect();
             }
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
-            using (SshClient linux = new SshClient("72.185.188.0", "mdeering", "Lolpops123"))
+            var currentServer = ServerSettingses.FirstOrDefault(x => x.Hostname == cmbServers.Text);
+
+            using (SshClient linux = new SshClient(currentServer?.Hostname, currentServer?.Username, currentServer?.Password))
             {
                 linux.RunCommand("cd /home/mdeering");
                 linux.RunCommand("./update.sh");
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            new AddServerWindow().ShowDialog();
+            LoadServers();
+        }
+
+        public List<ServerSettings> ServerSettingses { get; set; }
+
+
+        private void LoadServers()
+        {
+            if (!File.Exists("settings.bbq")) File.Create("settings.bbq").Close();
+            var serversText = File.ReadAllText("settings.bbq");
+            var servers = serversText.Split('\n');
+
+            if (ServerSettingses == null) ServerSettingses = new List<ServerSettings>();
+
+            cmbServers.Items.Clear();
+
+            foreach (var server in servers)
+            {
+                if (string.IsNullOrEmpty(server)) continue;
+
+                cmbServers.Items.Add(server.Split(':')[0]);
+                ServerSettingses.Add(new ServerSettings
+                {
+                    Hostname = server.Split(':')[0],
+                    Username = server.Split(':')[1],
+                    Password = server.Split(':')[2],
+                });
             }
         }
     }
